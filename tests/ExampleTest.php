ee3a670b8a55ffc4d4de5465f5da9c912c4b5b35
OscarWeijman <OscarWeijman@users.noreply.github.com>
2024-01-20 22:10:07 +0000

    Fix styling

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,6 +1,7 @@
 <?php
-use OscarWeijman\LaravelExample\Commands\LaravelExampleCommand;
+
 use Illuminate\Console\Command;
+use OscarWeijman\LaravelExample\Commands\LaravelExampleCommand;
 
 it('can test', function () {
     \Pest\Laravel\artisan(LaravelExampleCommand::class)->assertExitCode(Command::SUCCESS);
